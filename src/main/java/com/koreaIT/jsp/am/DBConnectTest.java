50953bf0df9b2263a2ceb5b271c33d6c591ecf1a
muk8620 <muk8620@gmail.com>
2024-06-11 15:00:59 +0900

    jsp_am 시작

---
+++ src/main/java/com/koreaIT/jsp/am/DBConnectTest.java
@@ -0,0 +1,63 @@
+package com.koreaIT.jsp.am;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.koreaIT.jsp.am.util.DBUtil;
+import com.koreaIT.jsp.am.util.SecSql;
+
+@WebServlet("/article/list")
+public class DBConnectTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		final String URL = "jdbc:mysql://localhost:3306/jsp_am?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+	    final String USER = "root";
+	    final String PASSWORD = "";
+	    
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+			SecSql sql = new SecSql();
+			sql.append("select * from article");
+			sql.append("order by id desc");
+			
+			List<Map<String, Object>> articleListMap = DBUtil.selectRows(connection, sql);
+			
+			request.setAttribute("articleListMap", articleListMap);
+			
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+			
+			System.out.println("연결 성공");
+			
+        } catch (SQLException e) {
+        	System.out.println("에러 : " + e);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+            if (connection != null) {
+            	try {
+            		connection.close();
+            	} catch (SQLException e) {
+            		e.printStackTrace();
+            	}
+            }
+        }
+	}
+
+}
